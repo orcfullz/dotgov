3d98ec24e3140a0b4aeeeb866d3b717dfa86780c
Alistair Norris <alistairnorris@Orcfull.local>
2018-10-20 19:57:44 +0800

    copying structure from agency site

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+// $(document).ready(function() {
+//         $('#terms').load('disclaimer.html');
+//         console.log('test');
+//                         alert("Please read the disclaimer.");
+
+//     });
+
+$( "#button1" ).click(function() {
+$('#terms-header').load('disclaimer.html');
+        console.log('homepage');
+                        // alert("This is the homepage");
+});
+
+$( "#button2" ).click(function() {
+$('#terms-header').load('disclaimertwo.html');
+        console.log('team page');
+                        // alert("This is the team page");
+});
+
+$( "#button3" ).click(function() {
+        $('#gifSpinner').show();
+
+
+$('#terms-header').load('disclaimerthree.html', function (){   $('#gifSpinner').hide();
+    console.log('loader shown');
+});
+        console.log('contact us');
+                        // alert("This is the contact us page");
+});
+
+ // $('#loading').show();
+
+ //    $("#content").load("example.html", function () { //calback function
+ //         $('#loading').hide();
+ //    });
+
+ $(window).scroll(function () {
+    console.log($(window).scrollTop());
+    var topDivHeight = $("#section-one").height();
+    var viewPortSize = $(window).height();
+
+    var triggerAt = 250;
+    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+
+    if ($(window).scrollTop() >= triggerHeight) {
+        $('.quote').css('visibility', 'visible').hide().fadeIn();
+        $(this).off('scroll');
+    }
+    });
